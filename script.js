7891b6fc6646349b6696d693ad0924223d9f73d7
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-12 12:30:23 -0800

    Added Multiplier

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+let multiplier = 1
+
 let clicks = 0
+
+function addToMuliplyer() {
+    if (clicks >= 10) {
+        clicks = clicks - 10;
+        multiplier++
+    }
+    document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
+    document.getElementById("multiplier").innerHTML = `Mulitplier is ${multiplier}x`
+    return [multiplier, clicks]
+}
 
 function count() {
 
-    clicks ++
+    addedAmount = 1 * multiplier;
+
+    clicks += addedAmount;
+
     document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
     return clicks
 
