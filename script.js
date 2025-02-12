c5ccc688499d19dbb7729c9885309603ecf58427
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-12 14:57:12 -0800

    Actually working and fun

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let multPrice = 10
 let multiplier = 1
 let clicks = 0
 let passiveClickers = 0
+let passivePrice = 100
 
 function addToMuliplyer() {
     if (clicks >= multPrice) {
         clicks = clicks - multPrice;
         multiplier++
         multPrice = Math.ceil(multPrice * 1.5 - 3)
-
     }
 
     document.getElementById("multPrice").innerHTML = `multiplier price is ${multPrice} clicks`
@@ -18,32 +18,33 @@ function addToMuliplyer() {
 }
 
 function count() {
-
     addedAmount = 1 * multiplier;
-
     clicks += addedAmount;
-
     document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
     return clicks
 
 }
 
 function passiveClickerP1() {
-    passiveClickers++
-    return passiveClickers
+    if (clicks >= passivePrice) {
+        clicks = clicks - passivePrice;
+        passiveClickers++
+        passivePrice = Math.ceil(passivePrice * 1.7 - 2)
+    }
+    document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
+    document.getElementById("passiveClickerPrice").innerHTML = `passive clicker price is ${passivePrice} clicks`
+    document.getElementById("passiveClicker").innerHTML = `Passive Clickers is ${passiveClickers}`
+
+    return [passiveClickers, clicks, passivePrice]
 }
 
 function passiveClickerP2() {
-
     // document.getElementById("passiveClickerPrice").innerHTML = `Passive Clickers is 0`
-
-    let compoundedClicks = passiveClickers * 10
-
+    let compoundedClicks = passiveClickers * 1
     clicks = clicks + compoundedClicks
-
     document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
 
     return clicks
 }
 
-setInterval(passiveClickerP2, 1000)
+setInterval(passiveClickerP2, 100)
