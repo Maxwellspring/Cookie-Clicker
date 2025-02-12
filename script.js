30d77687fb6bbc3e973de17c208e55ef38e31816
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-12 12:38:16 -0800

    added increasing cost

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+let price = 10
 let multiplier = 1
-
 let clicks = 0
 
 function addToMuliplyer() {
-    if (clicks >= 10) {
-        clicks = clicks - 10;
+    if (clicks >= price) {
+        clicks = clicks - price;
         multiplier++
+        price++
     }
     document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
     document.getElementById("multiplier").innerHTML = `Mulitplier is ${multiplier}x`
-    return [multiplier, clicks]
+    return [multiplier, clicks, price]
 }
 
 function count() {
