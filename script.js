5decdd79b4a5c43a9482614df291adee9c1dc09e
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-13 12:32:56 -0800

    Updated UI, added spent cookie count

--- script.js
+++ script.js
@@ -3,48 +3,45 @@ let multiplier = 1
 let clicks = 0
 let passiveClickers = 0
 let passivePrice = 100
+let spentClicks = 0
 
 function addToMuliplyer() {
     if (clicks >= multPrice) {
+        spentClicks = spentClicks - multPrice;
         clicks = clicks - multPrice;
         multiplier++
         multPrice = Math.ceil(multPrice * 1.5 - 3)
     }
-
     document.getElementById("multPrice").innerHTML = `multiplier price is ${multPrice} clicks`
-    document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
     document.getElementById("multiplier").innerHTML = `Mulitplier is ${multiplier}x`
-    return [multiplier, clicks, multPrice]
+    return [multiplier, clicks, multPrice, spentClicks]
 }
 
 function count() {
     addedAmount = 1 * multiplier;
     clicks += addedAmount;
-    document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
     return clicks
-
 }
 
 function passiveClickerP1() {
     if (clicks >= passivePrice) {
+        spentClicks = spentClicks - passivePrice;
         clicks = clicks - passivePrice;
         passiveClickers++
         passivePrice = Math.ceil(passivePrice * 1.7 - 2)
     }
-    document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
     document.getElementById("passiveClickerPrice").innerHTML = `passive clicker price is ${passivePrice} clicks`
     document.getElementById("passiveClicker").innerHTML = `Passive Clickers is ${passiveClickers}`
-
-    return [passiveClickers, clicks, passivePrice]
+    return [passiveClickers, clicks, passivePrice, spentClicks]
 }
 
-function passiveClickerP2() {
-    // document.getElementById("passiveClickerPrice").innerHTML = `Passive Clickers is 0`
+function updateDisplay() {
+    let fixedSpentClicks = spentClicks * -1
+    document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
+    document.getElementById("spent").innerHTML = `spent clicks is ${fixedSpentClicks} clicks`
     let compoundedClicks = passiveClickers * 1
     clicks = clicks + compoundedClicks
-    document.getElementById("count").innerHTML = `cookie clicks is ${clicks} clicks`
-
     return clicks
 }
 
-setInterval(passiveClickerP2, 100)
+setInterval(updateDisplay, 100)
